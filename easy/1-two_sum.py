49c7b04e205c212112c9a0e0ceaf90761e737f10
pcen <prscento@edu.uwaterloo.ca>
2020-01-19 18:02:08 -0500

    improve clarity of solutions, remove leetcode 'solution' classes

--- easy/1-two_sum.py
+++ easy/1-two_sum.py
@@ -1,16 +1,8 @@
-def driver():
-    s = Solution()
-    nums = [2, 7, 11, 15]
-    target = 9
-    print(s.twoSum(nums, target))
+def twoSum(nums, target):
+    paired_indexes = {}
+    for i, num in enumerate(nums):
+        if num in paired_indexes:
+            return [paired_indexes[num], i]
+        else:
+            paired_indexes[target - num] = i
 
-class Solution:
-    def twoSum(self, nums, target):
-        paired_indexes = {}
-        for i, num in enumerate(nums):
-            if num in paired_indexes:
-                return [paired_indexes[num], i]
-            else:
-                paired_indexes[target - num] = i
-
-driver()
